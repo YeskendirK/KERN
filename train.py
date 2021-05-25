2ade965cec30c00f39ba78d1764e3cd7cc759bcb
Yeskendir Koishekenov <yeskendir@Yeskendirs-MacBook-Pro.local>
2021-05-25 11:41:39 +0900

    Code refactoring

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import json
 import yaml
 import numpy as np
+from model.KERN_oms import KERN_oms
 from model.KERN import KERN
 from model.KERNGeoStyle import KERNGeoStyle
 import torch
@@ -18,6 +19,7 @@ from utility import increment_dir, select_device
 
 
 def train(conf, opt):
+    print("Starting training ... ")
     ori_path = "%s_%d__" %(conf["dataset"], conf["output_len"])
     settings = []
     if conf["use_grp_embed"] is False:
@@ -35,8 +37,17 @@ def train(conf, opt):
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
 
-    if conf["dataset"] in  ["fit", "omnious"]:
+
+    wdir = str(log_dir / 'weights') + os.sep  # weights directory
+    os.makedirs(wdir, exist_ok=True)
+    last = wdir + 'last.pt'
+    best = wdir + 'best.pt'
+    epochs = int(conf["epoch"])
+
+    if conf["dataset"]  == "fit":
         from utility import TrendDataset, TrendData
+    elif conf["dataset"]  == "omnious":
+        from utility_omnious import TrendDataset, TrendData
     else:
         from utility_geostyle import TrendDataset, TrendData
 
@@ -48,27 +59,67 @@ def train(conf, opt):
         conf["city_num"] = len(dataset.city_id_map)
         conf["gender_num"] = len(dataset.gender_id_map)
         conf["age_num"] = len(dataset.age_id_map)
+    elif conf["dataset"] == "omnious":
+        conf["location_num"] = len(dataset.location_id_map)
+        conf["segment_num"] = len(dataset.segment_id_map)
+        conf["target_age_num"] = len(dataset.target_age_id_map)
 
-    device = device = select_device(opt.device, batch_size=opt.batch_size)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = select_device(opt.device, batch_size=conf["batch_size"])
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     conf["device"] = device
 
     for k, v in conf.items():
         print(k, v)
 
-    if conf["dataset"] in ["fit", "omnious"]:
+    if conf["dataset"] == "fit":
         model = KERN(conf, dataset.adj)
+    elif conf["dataset"] == "omnious":
+        model = KERN_oms(conf)
     else:
         model = KERNGeoStyle(conf)
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=conf["lr"], weight_decay=1e-4)
-    #optimizer = torch.optim.Adam(model.parameters(), lr=conf["lr"])
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=int(conf["lr_decay_interval"]*len(dataset.train_set)/conf["batch_size"]), gamma=conf["lr_decay_gamma"])
+    pretrained = opt.weights.endswith('.pt')
+    if pretrained:
+        ckpt = torch.load(opt.weights, map_location=device)
+        state_dict = ckpt['model']
+        model.load_state_dict(state_dict, strict=False)
+        print('Transferred %g/%g items from %s' % (len(state_dict), len(model.state_dict()), opt.weights))
 
-    best_epoch, best_batch, best_mae, best_mape = 0, 0, 1, 100
-    best_val_mae, best_val_mape, best_test_mae, best_test_mape = 1, 100, 1, 100
-    for epoch in range(int(conf["epoch"])):
+    if conf["optimizer"] == "adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=conf["lr"], weight_decay=1e-4)
+    elif conf["optimizer"] == "sgd":
+        optimizer = torch.optim.SGD(model.parameters(), lr=conf["lr"], nesterov=True)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=conf["lr"])
+    if "lr_scheduler" not in conf or conf["lr_scheduler"] == "StepLR":
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=int(conf["lr_decay_interval"]*len(dataset.train_set)/conf["batch_size"]), gamma=conf["lr_decay_gamma"])
+    elif conf["lr_scheduler"] == "Plateau":
+        ttl_batch = len(dataset.train_set) / conf["batch_size"]
+        exp_lr_scheduler  = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', threshold_mode='abs', factor=0.5, patience=3*ttl_batch,
+                                                   verbose=True)
+
+    start_epoch = 0
+    if pretrained:
+        # Optimizer
+        if ckpt['optimizer'] is not None:
+            optimizer.load_state_dict(ckpt['optimizer'])
+
+
+        # Epochs
+        start_epoch = ckpt['epoch'] + 1
+        if epochs < start_epoch:
+            print('%s has been trained for %g epochs. Fine-tuning for %g additional epochs.' %
+                  (opt.weights, ckpt['epoch'], epochs))
+            epochs += ckpt['epoch']  # finetune additional epochs
+
+
+    if pretrained:
+        best_epoch, best_batch, best_mae, best_mape = -1, -1, ckpt["best_mae"], ckpt["best_mape"]
+        del ckpt, state_dict
+    else:
+        best_epoch, best_batch, best_mae, best_mape = 0, 0, float("inf"), float("inf")
+    best_val_mae, best_val_mape, best_test_mae, best_test_mape = float("inf"), float("inf"), float("inf"), float("inf")
+    for epoch in range(start_epoch, epochs):
         print("\nEpoch: %d" %(epoch))
         loss_print, enc_loss_print, dec_loss_print, triplet_loss_print = 0, 0, 0, 0
         pbar = tqdm(enumerate(dataset.train_loader), total=len(dataset.train_loader))
@@ -77,7 +128,7 @@ def train(conf, opt):
         for batch_i, batch in pbar:
         #for batch_i, batch in enumerate(dataset.train_loader):
             model.train(True)
-            exp_lr_scheduler.step() #adjust learning rate
+
             optimizer.zero_grad()
 
             self_cont, close_cont, far_cont, close_score, far_score = batch
@@ -96,12 +147,18 @@ def train(conf, opt):
                 loss = enc_loss + dec_loss
             loss.backward()                                                                                                
             optimizer.step()
+            if "lr_scheduler" in conf and conf["lr_scheduler"] == "Plateau":
+                exp_lr_scheduler.step(loss)  # adjust learning rate
+            else:
+                exp_lr_scheduler.step()
             
             loss_print += loss.item()
             enc_loss_print += enc_loss.item()
             dec_loss_print += dec_loss.item()
             triplet_loss_print += triplet_loss.item()
+            curr_lr = optimizer.param_groups[0]['lr']
 
+            log.add_scalar('parameters/learning_rate', curr_lr, epoch)
             log.add_scalar('Loss/train', loss.item(), batch_i + epoch * len(dataset.train_set)/conf["batch_size"])
             log.add_scalar('EncLoss/train', enc_loss.item(), batch_i + epoch * len(dataset.train_set)/conf["batch_size"])
             log.add_scalar('DecLoss/train', dec_loss.item(), batch_i + epoch * len(dataset.train_set)/conf["batch_size"])
@@ -118,10 +175,14 @@ def train(conf, opt):
                 log.add_scalar('MAE/test', test_mae, batch_i + epoch * ttl_batch)
                 log.add_scalar('MAPE/test', test_mape, batch_i + epoch * ttl_batch)
                 [loss_final, enc_loss_final, dec_loss_final] = test_loss_print
+
                 log.add_scalar('Loss/test', loss_final, batch_i + epoch * ttl_batch)
                 log.add_scalar('EncLoss/test', enc_loss_final, batch_i + epoch * ttl_batch)
                 log.add_scalar('DecLoss/test', dec_loss_final, batch_i + epoch * ttl_batch)
-                if mae <= best_mae and mape <= best_mape:
+
+                best_fitness = mae <= best_mae and mape <= best_mape
+
+                if best_fitness:
                     best_mae = mae
                     best_mape = mape
                     best_val_mae = val_mae
@@ -138,6 +199,31 @@ def train(conf, opt):
                        np.save("%s/city_embed_%s_%d" %(log_dir, conf["dataset"], conf["output_len"]), model.city_embeds.weight.detach().cpu().numpy())
                        np.save("%s/gender_embed_%s_%d" %(log_dir, conf["dataset"], conf["output_len"]), model.gender_embeds.weight.detach().cpu().numpy())
                        np.save("%s/age_embed_%s_%d" %(log_dir, conf["dataset"], conf["output_len"]), model.age_embeds.weight.detach().cpu().numpy())
+                    elif conf["dataset"] == "omnious":
+                        np.save("%s/location_embed_%s_%d" % (log_dir, conf["dataset"], conf["output_len"]),
+                                model.location_embeds.weight.detach().cpu().numpy())
+                        np.save("%s/segment_embed_%s_%d" % (log_dir, conf["dataset"], conf["output_len"]),
+                                model.segment_embeds.weight.detach().cpu().numpy())
+                        np.save("%s/target_age_embed_%s_%d" % (log_dir, conf["dataset"], conf["output_len"]),
+                                model.target_age_embeds.weight.detach().cpu().numpy())
+
+                save = (not opt.nosave)
+                final_epoch = epoch + 1 == epochs
+                if save:
+                    ckpt = {'epoch': epoch,
+                            'best_mae': best_mae,
+                            'best_mape': best_mape,
+                            'model': model.state_dict(),
+                            'optimizer': None if final_epoch else optimizer.state_dict()}
+
+                    # Save last, best and delete
+                    torch.save(ckpt, last)
+                    if epoch >= (epochs - 30):
+                        torch.save(ckpt, last.replace('.pt', '_{:03d}.pt'.format(epoch)))
+                    if best_fitness:
+                        torch.save(ckpt, best)
+                    del ckpt
+
                 print("MAE: %.6f, MAPE: %.6f, VAL_MAE: %.6f, VAL_MAPE: %.6f, TEST_MAE: %.6f, TEST_MAPE: %.6f" %(mae, mape, val_mae, val_mape, test_mae, test_mape))
                 print("BEST in epoch %d batch: %d, MAE: %.6f, MAPE: %.6f, VAL_MAE: %.6f, VAL_MAPE: %.6f, TEST_MAE: %.6f, TEST_MAPE: %.6f" %(best_epoch, best_batch, best_mae, best_mape, best_val_mae, best_val_mape, best_test_mae, best_test_mape))
 
@@ -165,8 +251,10 @@ def evaluate(model, dataset, device, conf):
 
         pbar.set_description('L:{:.6f}, EL:{:.6f}, DL:{:.6f}'.format(loss_print/(batch_i+1), enc_loss_print/(batch_i+1), dec_loss_print/(batch_i+1)))
 
-        if conf["dataset"] in ["fit", "omnious"]:
-            [input_seq, output_seq, grp_id, ele_id, norm, city_id, gender_id, age_id] = each_cont 
+        if conf["dataset"] == "fit":
+            [input_seq, output_seq, grp_id, ele_id, norm, city_id, gender_id, age_id] = each_cont
+        elif conf["dataset"] == "omnious":
+            [input_seq, output_seq, grp_id, ele_id, norm, location_id, segment_id, target_age_id] = each_cont
         else:
             [input_seq, output_seq, grp_id, ele_id, norm] = each_cont 
 
@@ -225,5 +313,8 @@ if __name__ == "__main__":
     parser.add_argument('--logdir', type=str, default='runs/', help='logging directory')
     parser.add_argument('--name', default='', help='adds name to default name if needed')
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or cpu')
+    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
+    parser.add_argument('--weights', type=str, default='', help='initial weights path')
+
     opt = parser.parse_args()
     main(opt)
