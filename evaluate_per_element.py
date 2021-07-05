a9944b6be71c1fa7d9172a73322f3327e4330d63
Yeskendir Koishekenov <yeskendir@Yeskendirs-MacBook-Pro.local>
2021-07-05 18:30:09 +0900

    Add evaluation per element

--- evaluate_per_element.py
+++ evaluate_per_element.py
@@ -1,24 +1,18 @@
-import os
-import json
-import yaml
-import math
-import numpy as np
 from collections import defaultdict
 
-from model.KERN_oms import KERN_oms
+import numpy as np
+import torch
+import yaml
+import csv
+
 from model.KERN import KERN
 from model.KERNGeoStyle import KERNGeoStyle
+from model.KERN_oms import KERN_oms
 
-import torch
 torch.manual_seed(1234)
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import lr_scheduler
 from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
 import argparse
-from pathlib import Path
-from utility import increment_dir, select_device
+from utility import select_device
 
 
 def main(opt):
@@ -30,11 +24,11 @@ def main(opt):
     print("Starting evaluating ...")
 
     if conf["dataset"] == "fit":
-        from utility import TrendDataset, TrendData
+        from utility import TrendData
     elif conf["dataset"] == "omnious":
-        from utility_omnious import TrendDataset, TrendData
+        from utility_omnious import TrendData
     else:
-        from utility_geostyle import TrendDataset, TrendData
+        from utility_geostyle import TrendData
 
     dataset = TrendData(conf)
     conf["grp_num"] = len(dataset.grp_id_map)
@@ -66,28 +60,36 @@ def main(opt):
         model.load_state_dict(state_dict, strict=False)
         print('Transferred %g/%g items from %s' % (len(state_dict), len(model.state_dict()), opt.weights))
 
-    mae_map, mape_map, perGroup_grd, perGroup_pred, all_train, val_mae_map, val_mape_map, test_mae_map, test_mape_map, test_loss_print = \
+    mae_map, mape_map, perElement_grd, perElement_pred, all_train, val_mae_map, val_mape_map, test_mae_map, test_mape_map, test_loss_print = \
         evaluate(model, dataset, device, conf)
 
-    grp_names = [key for key in dataset.grp_id_map.keys()]
+    ele_names = [key for key in dataset.ele_id_map.keys()]
 
+    # write to tsv file
+    with open("./evaluation_per_element.tsv", "wt") as out_file:
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(["GROUP", "MAE" , "MAPE" , "VAL_MAE" , "VAL_MAPE" , "TEST_MAE" , "TEST_MAPE"])
+        for ele_name in ele_names:
+            tsv_writer.writerow([ele_name, mae_map[ele_name], mape_map[ele_name], val_mae_map[ele_name], val_mape_map[ele_name],
+            test_mae_map[ele_name], test_mape_map[ele_name]])
+    '''        
     print("GROUP, MAE , MAPE , VAL_MAE , VAL_MAPE , TEST_MAE , TEST_MAPE ")
-    for grp_name in grp_names:
-        print( "%s, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f" % (
-            grp_name, mae_map[grp_name], mape_map[grp_name], val_mae_map[grp_name], val_mape_map[grp_name],
-            test_mae_map[grp_name], test_mape_map[grp_name]))
-
+    for ele_name in ele_names:
+        print("%s, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f" % (
+            ele_name, mae_map[ele_name], mape_map[ele_name], val_mae_map[ele_name], val_mape_map[ele_name],
+            test_mae_map[ele_name], test_mape_map[ele_name]))
+    '''
 
 def evaluate(model, dataset, device, conf):
     model.eval()
     pbar = tqdm(enumerate(dataset.test_loader), total=len(dataset.test_loader))
     loss_print, enc_loss_print, dec_loss_print = 0, 0, 0
     all_grd, all_pred, all_norm = [], [], []
-    perGroup_grd, perGroup_pred, perGroup_norm = defaultdict(list), defaultdict(list), defaultdict(list)
+    perElement_grd, perElement_pred, perElement_norm = defaultdict(list), defaultdict(list), defaultdict(list)
 
-    grp_name_map = {}
-    for grp_name, grp_id in dataset.grp_id_map.items():
-        grp_name_map[grp_id] = grp_name
+    ele_name_map = {}
+    for ele_name, ele_id in dataset.ele_id_map.items():
+        ele_name_map[ele_id] = ele_name
 
     for batch_i, batch in pbar:
         each_cont = [each.to(device) for each in batch]
@@ -105,96 +107,90 @@ def evaluate(model, dataset, device, conf):
         pbar.set_description(
             'L:{:.6f}, EL:{:.6f}, DL:{:.6f}'.format(loss_print / (batch_i + 1), enc_loss_print / (batch_i + 1),
                                                     dec_loss_print / (batch_i + 1)))
+
         if conf["dataset"] == "fit":
-            [input_seq, output_seq, grp_ids, ele_id, norm, city_id, gender_id, age_id] = each_cont
+            [input_seq, output_seq, grp_ids, ele_ids, norm, city_id, gender_id, age_id] = each_cont
         elif conf["dataset"] == "omnious":
-            [input_seq, output_seq, grp_ids, ele_id, norm, location_id, segment_id, target_age_id] = each_cont
+            [input_seq, output_seq, grp_ids, ele_ids, norm, location_id, segment_id, target_age_id] = each_cont
         else:
-            [input_seq, output_seq, grp_ids, ele_id, norm] = each_cont
+            [input_seq, output_seq, grp_ids, ele_ids, norm] = each_cont
 
         all_grd.append(output_seq[:, :, 1].cpu())
         all_pred.append(pred.cpu())
         all_norm.append(norm.cpu())
         all_train = dataset.all_train_seq
 
-        for i in range(len(grp_ids)):
-            grp_id = grp_ids[i][0].item()
-            grp_name = grp_name_map[grp_id]
-            perGroup_grd[grp_name].append(torch.stack([output_seq[i, :, 1].cpu()]))
-            perGroup_pred[grp_name].append(torch.stack([pred[i].cpu()]))
-            perGroup_norm[grp_name].append(torch.stack([norm[i].cpu()]))
+        for i in range(len(ele_ids)):
+            ele_id = ele_ids[i][0].item()
+            ele_name = ele_name_map[ele_id]
+            perElement_grd[ele_name].append(torch.stack([output_seq[i, :, 1].cpu()]))
+            perElement_pred[ele_name].append(torch.stack([pred[i].cpu()]))
+            perElement_norm[ele_name].append(torch.stack([norm[i].cpu()]))
 
     all_grd = torch.cat(all_grd, dim=0).numpy()
     all_pred = torch.cat(all_pred, dim=0).detach().numpy()
     all_norm = torch.cat(all_norm, dim=0).detach().numpy()
     all_train = dataset.all_train_seq
 
-    for grp_name in perGroup_grd.keys():
-        perGroup_grd[grp_name] = torch.cat(perGroup_grd[grp_name], dim=0).numpy()
-        perGroup_pred[grp_name] = torch.cat(perGroup_pred[grp_name], dim=0).detach().numpy()
-        perGroup_norm[grp_name] = torch.cat(perGroup_norm[grp_name], dim=0).detach().numpy()
+    for ele_name in perElement_grd.keys():
+        perElement_grd[ele_name] = torch.cat(perElement_grd[ele_name], dim=0).numpy()
+        perElement_pred[ele_name] = torch.cat(perElement_pred[ele_name], dim=0).detach().numpy()
+        perElement_norm[ele_name] = torch.cat(perElement_norm[ele_name], dim=0).detach().numpy()
 
     if conf["denorm"] is True:
         all_grd = denorm(all_grd, all_norm)
         all_pred = denorm(all_pred, all_norm)
         all_train = denorm(all_train, all_norm)
 
-        for grp_name in perGroup_grd.keys():
-            perGroup_grd[grp_name] = denorm(perGroup_grd[grp_name], perGroup_norm[grp_name])
-            perGroup_pred[grp_name] = denorm(perGroup_pred[grp_name], perGroup_norm[grp_name])
-
-    # perGroup_grd['all'] = all_grd
-    # perGroup_pred['all'] = all_pred
+        for ele_name in perElement_grd.keys():
+            perElement_grd[ele_name] = denorm(perElement_grd[ele_name], perElement_norm[ele_name])
+            perElement_pred[ele_name] = denorm(perElement_pred[ele_name], perElement_norm[ele_name])
 
     val_pred = all_pred[::2]
     val_grd = all_grd[::2]
     val_pred_map, val_grd_map = {}, {}
-    # val_pred_map['all'] = val_pred
-    # val_grd_map['all'] = val_grd
-    for grp_name in perGroup_pred.keys():
-        val_pred_map[grp_name] = perGroup_pred[grp_name][::2]
-        val_grd_map[grp_name] = perGroup_grd[grp_name][::2]
+    for ele_name in perElement_pred.keys():
+        val_pred_map[ele_name] = perElement_pred[ele_name][::2]
+        val_grd_map[ele_name] = perElement_grd[ele_name][::2]
 
     test_pred = all_pred[1::2]
     test_grd = all_grd[1::2]
     test_pred_map, test_grd_map = {}, {}
-    # test_pred_map['all'] = test_pred
-    # test_grd_map['all'] = test_grd
-    for grp_name in perGroup_grd.keys():
-        test_pred_map[grp_name] = perGroup_pred[grp_name][1::2]
-        test_grd_map[grp_name] = perGroup_grd[grp_name][1::2]
+
+    for ele_name in perElement_grd.keys():
+        test_pred_map[ele_name] = perElement_pred[ele_name][1::2]
+        test_grd_map[ele_name] = perElement_grd[ele_name][1::2]
 
     mae = np.mean(np.abs(all_pred - all_grd))
     mape = np.mean(np.abs(all_pred - all_grd) / all_grd) * 100
     mae_map, mape_map = {}, {}
     # mae_map["all"] = mae
     # mape_map['all'] = mape
-    for grp_name in perGroup_pred.keys():
-        mae_map[grp_name] = np.mean(np.abs(perGroup_pred[grp_name] - perGroup_grd[grp_name]))
-        mape_map[grp_name] = np.mean(
-            np.abs(perGroup_pred[grp_name] - perGroup_grd[grp_name]) / perGroup_grd[grp_name]) * 100
+    for ele_name in perElement_pred.keys():
+        mae_map[ele_name] = np.mean(np.abs(perElement_pred[ele_name] - perElement_grd[ele_name]))
+        mape_map[ele_name] = np.mean(
+            np.abs(perElement_pred[ele_name] - perElement_grd[ele_name]) / perElement_grd[ele_name]) * 100
 
     val_mae = np.mean(np.abs(val_pred - val_grd))
     val_mape = np.mean(np.abs(val_pred - val_grd) / val_grd) * 100
     val_mae_map, val_mape_map = {}, {}
     # val_mae_map['all'] = val_mae
     # val_mape_map['all'] = val_mape
-    for grp_name in perGroup_pred.keys():
-        val_mae_map[grp_name] = np.mean(np.abs(val_pred_map[grp_name] - val_grd_map[grp_name]))
-        val_mape_map[grp_name] = np.mean(
-            np.abs(val_pred_map[grp_name] - val_grd_map[grp_name]) / val_grd_map[grp_name]) * 100
+    for ele_name in perElement_pred.keys():
+        val_mae_map[ele_name] = np.mean(np.abs(val_pred_map[ele_name] - val_grd_map[ele_name]))
+        val_mape_map[ele_name] = np.mean(
+            np.abs(val_pred_map[ele_name] - val_grd_map[ele_name]) / val_grd_map[ele_name]) * 100
 
     test_mae = np.mean(np.abs(test_pred - test_grd))
     test_mape = np.mean(np.abs(test_pred - test_grd) / test_grd) * 100
     test_mae_map, test_mape_map = {}, {}
-    # test_mae_map['all'] = test_mae
-    # test_mape_map['all'] = test_mape
-    for grp_name in perGroup_grd.keys():
-        test_mae_map[grp_name] = np.mean(np.abs(test_pred_map[grp_name] - test_grd_map[grp_name]))
-        test_mape_map[grp_name] = np.mean(
-            np.abs(test_pred_map[grp_name] - test_grd_map[grp_name]) / test_grd_map[grp_name]) * 100
 
-    return mae_map, mape_map, perGroup_grd, perGroup_pred, all_train, val_mae_map, val_mape_map, test_mae_map, test_mape_map, [
+    for ele_name in perElement_grd.keys():
+        test_mae_map[ele_name] = np.mean(np.abs(test_pred_map[ele_name] - test_grd_map[ele_name]))
+        test_mape_map[ele_name] = np.mean(
+            np.abs(test_pred_map[ele_name] - test_grd_map[ele_name]) / test_grd_map[ele_name]) * 100
+
+    return mae_map, mape_map, perElement_grd, perElement_pred, all_train, val_mae_map, val_mape_map, test_mae_map, test_mape_map, [
         loss_final, enc_loss_final, dec_loss_final]
 
 
